bd3aa0a5643d17a310700fdf306b9733cb045ea4
Matthew Gall <me@matthewgall.com>
2018-04-04 22:16:30 +0100

    Added new routes for /ip and /headers

--- app.js
+++ app.js
@@ -8,17 +8,35 @@ addEventListener('fetch', event => {
  */
 async function fetchAndRespond(request) {
 
-    reqUrl = new URL(request.url).pathname
-    
+    let reqUrl = new URL(request.url).pathname
+
     data = {
       'success': true,
       'results': {
-        'ip': request.headers.get('Cf-Connected-Ip')
+        'ip': request.headers.get('Cf-Connecting-Ip'),
+        'headers': {}
       }
     }
 
-    if (reqUrl == '/') {
+    for (let entry of request.headers.entries()) {
+      data['results']['headers'][entry[0]] = entry[1]
+    }
+    
+    if (reqUrl == '/' || reqUrl == '/ip') {
       return new Response(data['results']['ip'], {
+        headers: {
+          'Content-Type': 'text/plain'
+        }
+      })
+    }
+    if (reqUrl == '/headers') {
+      resp = []
+
+      for (let entry in data['results']['headers']) {
+        resp.push(entry + ': ' + data['results']['headers'][entry])
+      }
+      
+      return new Response(resp.join('\r\n'), {
         headers: {
           'Content-Type': 'text/plain'
         }
